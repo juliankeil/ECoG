ee5a23472cff2e08c903fd252bb22a36001ac7e4
juliankeil <julian.keil@gmail.com>
2018-01-24 09:20:41 +0100

    Updated postprocessing

--- KEH_test.m
+++ KEH_test.m
@@ -7,7 +7,12 @@ clc
 
 % 2.1. Set Data Directory
 indata(1) = dir('E:\ECoG\Raw\DW001\KO171120_160637_2.BDF');
+intrig(1) = 70;
+inref(1) = 67; 
 indata(2) = dir('E:\ECoG\Raw\A0058\a0058.BDF');
+intrig(2) = 54;
+inref(2) = 52;
+
 
 indir{1} = 'E:\ECoG\Raw\DW001\';
 indir{2} = 'E:\ECoG\Raw\A0058\';
@@ -27,7 +32,7 @@ downsamplefreq = 600;
 % 3.2. Use EEGLab to read in the data, filter and downsample
 for v = 1:length(indata)
    % 3.2.1. Load Data
-   EEG_raw = pop_readbdf([indir{v} indata(v).name], [], 70, 67'); % Read in section of data
+   EEG_raw = pop_readbdf([indir{v} indata(v).name], [], intrig(v), inref(v)'); % Read in section of data
    EEG_raw = pop_rmbase( EEG_raw, []); % Remove Mean of each channel
    EEG_raw.data = double(EEG_raw.data);
 
@@ -36,10 +41,13 @@ for v = 1:length(indata)
    EEG_filt = pop_eegfiltnew(EEG_filt,[],lpfreq,[],[],[],plotResponse); % for different transition bands
    EEG_filt = pop_eegfiltnew(EEG_filt,notchfreq-1,notchfreq+1,[],1,[],plotResponse);
 
-   % 3.2.3. Downsample
+   % 3.2.3. Downsample and Save
    EEG_filt = pop_resample(EEG_filt, downsamplefreq);
-   EEG_filt = pop_saveset(EEG_filt,'filename',[indata(v).name,'_filt'],'filepath',outdir,'savemode','onefile','version','7.3');
-
+   EEG_filt = pop_saveset(EEG_filt,'filename',[indata(v).name],'filepath',outdir,'savemode','onefile','version','7.3');
+end
+%% Load Preprocessed Data
+for v = 1:length(indata)
+   EEG_filt = pop_loadset('filename',[indata(v).name(1:end-4),'.set'],'filepath',outdir); 
    % 3.2.4. Export to FT
    dat{v} = eeglab2fieldtrip(EEG_filt,'preprocessing','none');
    dat{v}.event = EEG_filt.event;
@@ -56,80 +64,89 @@ for v = 1:length(indata)
 end
 
 %% Post-Processing
-v = 1; % Set for Dataset
-%% 5. Redefinetrial
-cfg = [];
-cfg.trl = dat{v}.cfg.trl;
+for v = 1:length(dat); % Set for Dataset
+    %% 5. Redefinetrial
+    cfg = [];
+    cfg.trl = dat{v}.cfg.trl;
 
-dat_trl{v} = ft_redefinetrial(cfg,dat{v});
+    dat_trl{v} = ft_redefinetrial(cfg,dat{v});
+
+%     %%
+%     cfg = [];
+%     cfg.viewmode = 'vertical';
+%     ft_databrowser([],dat_trl{v});
+
+    %%
+    vis = find(dat_trl{v}.trialinfo == 33);
+    aud = find(dat_trl{v}.trialinfo == 40);
+    sifi = find(dat_trl{v}.trialinfo == 41);
+
+    cfg = [];
+    cfg.trials = vis;
+    ERP_v{v} = ft_timelockanalysis(cfg,dat_trl{v});
+
+    cfg.trials = aud;
+    ERP_a{v} = ft_timelockanalysis(cfg,dat_trl{v});
+
+    cfg.trials = sifi;
+    ERP_s{v} = ft_timelockanalysis(cfg,dat_trl{v});
+
+    cfg = [];
+    cfg.baseline = [-1 1];
+    cfg.channel = {'all','-EKG','-REF','-Trigger1'};%1:66;
+
+    ERP_bl_v{v} = ft_timelockbaseline(cfg,ERP_v{v});
+    ERP_bl_a{v} = ft_timelockbaseline(cfg,ERP_a{v});
+    ERP_bl_s{v} = ft_timelockbaseline(cfg,ERP_s{v});
+
+    cfg = [];
+    cfg.operation = 'x1-(x2+x3)';
+    cfg.parameter = 'avg';
+    ERP_bl_add{v} = ft_math(cfg,ERP_bl_s{v},ERP_bl_v{v},ERP_bl_a{v});
+
+    % Find the Channel with the strongest superadditive response (A1V1 - (A1 + V1))
+    for c = 1:(size(ERP_bl_add{v}.avg,1)-3)
+        [val(c) time(c)] = min(ERP_bl_add{v}.avg(c));
+    end
+    [minval(v) minchan(v)] = min(val);
+
+    cfg = [];
+    cfg.channel = minchan(v);
+    figure;ft_singleplotER(cfg,ERP_bl_v{v},ERP_bl_a{v},ERP_bl_s{v});
+
+    cfg = [];
+    cfg.channel = minchan(v);
+    figure;ft_singleplotER(cfg,ERP_bl_add{v});
+
+end
 
 %%
-cfg = [];
-cfg.viewmode = 'vertical';
-ft_databrowser([],dat_trl{v});
+for v = 1:length(dat);
+    cfg=[];
+    cfg.method='wavelet';
+    cfg.output='pow'; % output parameter
+    cfg.foi=[3:3:150];
+    cfg.toi=[-.5:.01:.5];
+    cfg.width = 5;
+    cfg.pad='nextpow2';
 
-%%
-vis = find(dat_trl{v}.trialinfo == 33);
-aud = find(dat_trl{v}.trialinfo == 40);
-sifi = find(dat_trl{v}.trialinfo == 41);
+    cfg.trials = vis;
+    WLT_trl_v{v}=ft_freqanalysis(cfg,dat_trl{v});
+    cfg.trials = aud;
+    WLT_trl_a{v}=ft_freqanalysis(cfg,dat_trl{v});
+    cfg.trials = sifi;
+    WLT_trl_s{v}=ft_freqanalysis(cfg,dat_trl{v});
 
-cfg = [];
-cfg.trials = vis;
-ERP_v{v} = ft_timelockanalysis(cfg,dat_trl{v});
+    cfg = [];
+    cfg.channel = minchan(v);
+    cfg.baseline = [-.5 0];
+    cfg.baselinetype = 'db';
+    cfg.zlim = [- 5 5];
 
-cfg.trials = aud;
-ERP_a{v} = ft_timelockanalysis(cfg,dat_trl{v});
-
-cfg.trials = sifi;
-ERP_s{v} = ft_timelockanalysis(cfg,dat_trl{v});
-
-cfg = [];
-cfg.baseline = [-1 1];
-cfg.channel = 1:66;
-
-ERP_bl_v{v} = ft_timelockbaseline(cfg,ERP_v{v});
-ERP_bl_a{v} = ft_timelockbaseline(cfg,ERP_a{v});
-ERP_bl_s{v} = ft_timelockbaseline(cfg,ERP_s{v});
-
-cfg = [];
-cfg.channel = 27;
-figure;ft_singleplotER(cfg,ERP_bl_v{v},ERP_bl_a{v},ERP_bl_s{v});
-
-cfg = [];
-cfg.operation = 'x1-(x2+x3)';
-cfg.parameter = 'avg';
-ERP_bl_add{v} = ft_math(cfg,ERP_bl_s{v},ERP_bl_v{v},ERP_bl_a{v});
-
-cfg = [];
-cfg.channel = 27;
-figure;ft_singleplotER(cfg,ERP_bl_add{v});
-%%
-
-cfg=[];
-cfg.channel = 1:66;
-cfg.method='wavelet';
-cfg.output='pow'; % output parameter
-cfg.foi=[3:3:150];
-cfg.toi=[-.5:.01:.5];
-cfg.width = 5;
-cfg.pad='nextpow2';
-
-cfg.trials = vis;
-WLT_trl_v{v}=ft_freqanalysis(cfg,dat_trl{v});
-cfg.trials = aud;
-WLT_trl_a{v}=ft_freqanalysis(cfg,dat_trl{v});
-cfg.trials = sifi;
-WLT_trl_s{v}=ft_freqanalysis(cfg,dat_trl{v});
-
-cfg = [];
-cfg.channel = 27;
-cfg.baseline = [-.5 0];
-cfg.baselinetype = 'db';
-cfg.zlim = [- 5 5];
-
-figure;ft_singleplotTFR(cfg,WLT_trl_v{v});
-figure;ft_singleplotTFR(cfg,WLT_trl_a{v});
-figure;ft_singleplotTFR(cfg,WLT_trl_s{v});
+    figure;ft_singleplotTFR(cfg,WLT_trl_v{v});
+    figure;ft_singleplotTFR(cfg,WLT_trl_a{v});
+    figure;ft_singleplotTFR(cfg,WLT_trl_s{v});
+end
 %% Search Triggers and Latencies
 for i = 1:length(EEG_raw.event)
 trig(i) =  EEG_raw.event(i).type;
